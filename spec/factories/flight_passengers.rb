0dfbbaab8ec06008ad21a1aa0c9ede117cd6a2e6
Jessica Jones <daddehjones@gmail.com>
2021-09-24 10:24:07 -0400

    Add orderly to gemfile. Create migrations and models.

---
+++ spec/factories/flight_passengers.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :flight_passenger do
+    flight { nil }
+    passenger { nil }
+  end
+end
---
+++ spec/factories/passengers.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :passenger do
+    name { "MyString" }
+    age { 1 }
+    flight { nil }
+  end
+end
